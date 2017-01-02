c020e08a371ffba2641e733bf1d50119b778d9dd
Andy Richardson <contact@andythedeveloper.com>
2017-01-02 17:33:29 +0000

    Import packaged class from non packaged class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+import single.PackagedClass;
+
 class HelloWorld {
   public static void main(String[] args) {
     System.out.println("hello world");
+    System.out.print(PackagedClass.sayHello());
   }
 }
--- single/PackagedClass.java
+++ single/PackagedClass.java
@@ -1,6 +1,10 @@
 package single;
 
 public class PackagedClass {
+  public static String sayHello(){
+    return "hello from the packaged class";
+  }
+
   public static void main(String[] args) {
     System.out.print("Hello packaged world");
   }
